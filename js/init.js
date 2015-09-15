35a4ba7a2ebfc69c75e82d5001531bf32ff9b939
TJ Buttner <tjsight@gmail.com>
2015-09-15 18:14:46 -0400

    changes of original
    these are changes from the original code

---
+++ js/init.js
@@ -0,0 +1,8 @@
+(function($){
+
+    $('.button-collapse').sideNav();
+    $('.collapsible').collapsible();
+    $('.parallax').parallax();
+    $('.dropdown-button').dropdown({hover: false});
+
+})(jQuery); // end of jQuery name space
